8b44ee606f1d7b51e85c26ed8c0215289cc56e40
Nogueira Jr <nogueirajr@hotmail.com>
2024-07-30 08:38:28 -0300

    Lab Producer 03

---
+++ lab/producer03.py
@@ -0,0 +1,61 @@
+import pika
+import json
+
+# Conexão com o RabbitMQ
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+
+# Declarar a fila
+channel.queue_declare(queue='hello')
+
+# Mensagem a ser enviada
+message = {
+    "endpoint": "http://localhost:3333/user",
+    "headers": {
+        "Content-Type": "application/json"
+    },
+    "payload": {
+        "name": "Ciclano",
+        "usr": "usrCiclanoComSenha3",
+        "psw": "SenhaDoCiclano3",
+        "emails": [
+            {
+                "email": "endereco@email.com",
+                "userId": "idAdmin000",
+                "userEmailTypeId": "others",
+                "active": True
+            },
+            {
+                "email": "endereco2@email.com",
+                "userId": "idAdmin000",
+                "userEmailTypeId": "others",
+                "active": True
+            }
+        ],
+        "phones": [
+            {
+                "phone": "12489156131",
+                "userId": "idAdmin000",
+                "userPhoneTypeId": "others",
+                "active": True
+            },
+            {
+                "phone": "1248915613123465",
+                "userId": "idAdmin000",
+                "userPhoneTypeId": "others",
+                "active": True
+            }
+        ],
+        "userTypeId": "admin"
+    }
+}
+
+# Enviar a mensagem para a fila
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body=json.dumps(message))
+
+print("Mensagem enviada com sucesso!")
+
+# Fechar a conexão
+connection.close()
