58e074bcf17b9436bb657e8ea97a7ce56343bfa7
Nogueira Jr <nogueirajr@hotmail.com>
2024-09-05 12:11:17 -0300

    Lab Flow 04 Payload

---
+++ lab/Flow04/payloads.py
@@ -0,0 +1,83 @@
+def payload(id, seq, connection):
+    initial_payload = {
+        "endPoint": "user",
+        "payload": {
+            "name": "Ciclano",
+            "usr": "usrCiclanoComSenha3",
+            "psw": "SenhaDoCiclano3",
+            "emails": [
+                {
+                    "email": "endereco@email.com",
+                    "userId": "idAdmin000",
+                    "userEmailTypeId": "others",
+                    "active": True
+                },
+                {
+                    "email": "endereco2@email.com",
+                    "userId": "idAdmin000",
+                    "userEmailTypeId": "others",
+                    "active": True
+                }
+            ],
+            "phones": [
+                {
+                    "phone": "12489156131",
+                    "userId": "idAdmin000",
+                    "userPhoneTypeId": "others",
+                    "active": True
+                },
+                {
+                    "phone": "1248915613123465",
+                    "userId": "idAdmin000",
+                    "userPhoneTypeId": "others",
+                    "active": True
+                }
+            ],
+            "userTypeId": "admin"
+        }
+    }
+
+    if seq == 0:
+        return initial_payload
+    elif seq == 1:
+        return {
+            "endPoint": "user",
+            "payload": {
+                "userId": id,
+                "name": "Fulano",
+                "usr": "usrFulanoComSenha4",
+                "psw": "SenhaDoFulano4",
+                "emails": [
+                    {
+                        "email": "outroendereco@email.com",
+                        "userId": "idUser001",
+                        "userEmailTypeId": "personal",
+                        "active": False
+                    },
+                    {
+                        "email": "outroendereco2@email.com",
+                        "userId": "idUser001",
+                        "userEmailTypeId": "work",
+                        "active": False
+                    }
+                ],
+                "phones": [
+                    {
+                        "phone": "9876543210",
+                        "userId": "idUser001",
+                        "userPhoneTypeId": "personal",
+                        "active": False
+                    },
+                    {
+                        "phone": "9876543210987",
+                        "userId": "idUser001",
+                        "userPhoneTypeId": "work",
+                        "active": False
+                    }
+                ],
+                "userTypeId": "user"
+            }
+        }
+    else:
+        # connection.close()
+        return None
