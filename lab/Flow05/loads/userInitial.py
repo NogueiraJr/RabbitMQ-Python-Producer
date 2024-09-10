a3d14898f0294e603c8a664e66534debff188d66
Nogueira Jr <nogueirajr@hotmail.com>
2024-09-10 09:16:15 -0300

    Lab Flow 05 User Initial

---
+++ lab/Flow05/loads/userInitial.py
@@ -0,0 +1,38 @@
+def json_userInitial():
+    return {
+        "endPoint": "user",
+        "payload": {
+            "name": "Ciclano",
+            "usr": "usrCiclanoComSenha3",
+            "psw": "SenhaDoCiclano3",
+            "emails": [
+                {
+                    "email": "endereco@email.com",
+                    "userId": "idAdmin000",
+                    "userEmailTypeId": "others",
+                    "active": True
+                },
+                {
+                    "email": "endereco2@email.com",
+                    "userId": "idAdmin000",
+                    "userEmailTypeId": "others",
+                    "active": True
+                }
+            ],
+            "phones": [
+                {
+                    "phone": "12489156131",
+                    "userId": "idAdmin000",
+                    "userPhoneTypeId": "others",
+                    "active": True
+                },
+                {
+                    "phone": "1248915613123465",
+                    "userId": "idAdmin000",
+                    "userPhoneTypeId": "others",
+                    "active": True
+                }
+            ],
+            "userTypeId": "admin"
+        }
+    }
