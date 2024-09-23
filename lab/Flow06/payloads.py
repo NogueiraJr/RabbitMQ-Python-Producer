fbf82c0fe102869957195d7fc853fc6ca835d862
Nogueira Jr <nogueirajr@hotmail.com>
2024-09-23 09:47:45 -0300

    Lab Flow 06 Payload

--- lab/Flow06/payloads.py
+++ lab/Flow06/payloads.py
@@ -1,6 +1,7 @@
 from loads.userCreateInitial import json_userCreateInitial
 from loads.userCreateChild import json_userCreateChild
 from loads.userGetCollection import json_userGetCollection
+from loads.productCreate import json_productCreate
 
 def payload(id, seq, connection):
     if seq == 0:
@@ -9,6 +10,9 @@ def payload(id, seq, connection):
         return json_userCreateChild(id)
     elif seq == 2:
         return json_userGetCollection(id)
+    elif seq == 3:
+        return json_productCreate(id)
     else:
         connection.close()
         return None
+
